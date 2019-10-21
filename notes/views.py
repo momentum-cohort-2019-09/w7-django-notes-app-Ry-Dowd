51bfee63c83fbbd85e16733597974caf6c490085
Ryan <rydowd@gmail.com>
2019-10-21 13:20:39 -0400

    tentatively implemented views

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from notes.data import NOTES
 
 # Create your views here.
+def notes_view(request):
+  return render(request, "notes/notes_view.html", {
+    "notes": NOTES,
+  })
+  
+def notes_detail(request, id):
+  note = NOTES[id]
+  return render(request, "notes/notes_detail.html", {
+    "note" : note
+  })
