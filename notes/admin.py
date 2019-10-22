2faf8b7be69c593259897fde550b6da4ac77ff6e
Ryan <rydowd@gmail.com>
2019-10-22 17:25:28 -0400

    updated styling and improved admin interface for notes

--- notes/admin.py
+++ notes/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Note
+from .models import Note, Comment
 
-admin.site.register(Note)
+class CommentInLine(admin.StackedInline):
+  model=Comment
+  extra=2
+  
+class NoteAdmin(admin.ModelAdmin):
+  list_display=('title', 'updated')
+  inlines=[CommentInLine]
+
+admin.site.register(Note, NoteAdmin)
+admin.site.register(Comment)
--- notes/models.py
+++ notes/models.py
@@ -2,11 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Note(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(help_text="This is a note")
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+  title = models.CharField(max_length=255)
+  content = models.TextField(help_text="This is a note")
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.title
-
+  def __str__(self):
+    return self.title
+        
+class Comment(models.Model):
+  content = models.CharField(max_length=255)
+  note = models.ForeignKey(to=Note, on_delete=models.CASCADE, related_name='comments')
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
+  
+  def __str__(self):
+    return self.content
