64f4914eb49d44905438c43cb8d31a4aafeff2cf
Ryan <rydowd@gmail.com>
2019-10-23 16:57:21 -0400

    created form for comments, not yet linked

--- notes/forms.py
+++ notes/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from notes.models import Note
+from notes.models import Note, Comment
 
 class NoteForm(forms.ModelForm):
   class Meta:
     model = Note
     fields = {'title', 'content'}
+    
+class CommentForm(forms.ModelForm):
+  class Meta:
+    model = Comment
+    fields = {'content',}
